a7b33764de20aa84345dfcfa69055fd08e679637
DenysHnatiuk <denys.hnatiuk@gmail.com>
2023-06-10 11:43:52 +0300

    phpdoc pb class

--- inc/classes/class-pb.php
+++ inc/classes/class-pb.php
@@ -17,16 +17,23 @@ if ( ! class_exists( 'Pb' ) ) {
 	
 	final class Pb {
 
-		// TODO: is there security profit to use singleton?
-
-		// private static $instance;
-		// public static function instance() {
-		// 	if ( is_null( self::$instance ) ) {
-		// 		self::$instance = new self;
-		// 	}
-		// 	return self::$instance;
-		// }
-
+		/**
+		 * TODO: is there security profit to use singleton?
+		 * private static $instance;
+		 * public static function instance() {
+		 * 	if ( is_null( self::$instance ) ) {
+		 * 		self::$instance = new self;
+		 * 	}
+		 * 	return self::$instance;
+		 * }
+		 */
+				
+		/**
+		 * __construct
+		 *
+		 * @param array<string, string|mixed>  $theme_config
+		 * @return void
+		 */
 		public function __construct( $theme_config ) {
 			if ( ! version_compare( PHP_VERSION, '8.0', '>=' ) ) {
 				$this->action_admin_notice(
@@ -55,18 +62,29 @@ if ( ! class_exists( 'Pb' ) ) {
 		// public function __clone() {
 		//   _doing_it_wrong(__FUNCTION__, __('Cheatin&#8217; huh?', 'text_domain_example' ), '1.0' );
 		// }
-
-		private function extend_and_override_defaults( $theme_config ) {
+		
+		/**
+		 * extend_and_override_defaults
+		 *
+		 * @param array<string, string|mixed>  $theme_config
+		 * @return array<string, string|mixed>  $theme_config
+		 */
+		private function extend_and_override_defaults( array $theme_config ) {
 			// TODO: maybe use other notation. js with comments or thmth else
-			$default_config = new RecursiveArrayIterator( json_decode( file_get_contents( get_template_directory() . '/inc/config/pb.config.json' ), true ) );
+      $config_path = get_template_directory() . '/inc/config/pb.config.json';
+      /**
+       * @var array<string, string> $default_config 
+       */
+			$default_config = new RecursiveArrayIterator( json_decode( file_get_contents( $config_path ),true ) );
 
 			if ( ! empty( $theme_config ) && is_array( $theme_config ) ) {
+				/**
+				 * @var array<string, string> $overrides
+				 */
 				$overrides = new RecursiveIteratorIterator( new RecursiveArrayIterator( $theme_config ) );
 
 				foreach ( $overrides as $key => $value ) {
-					if ( array_key_exists( $key, $default_config ) ) {
-						$default_config->$key = $value;
-					}
+					if ( array_key_exists( $key, $default_config ) ) { $default_config->$key = $value; }
 					// if ( is_array( $value ) && array_key_exists( 'special_key', $value ) ) {
 					// 	// Here we replace ALL keys with the same value from 'special_key'
 					// 	$replaced = array_fill( 0, count( $value ), $value['special_key'] );
@@ -98,8 +116,19 @@ if ( ! class_exists( 'Pb' ) ) {
 			}
 			return $default_config;
 		}
+		
+		/**
+		 * Setup loop parses theme config json object
+		 *
+		 * @param  array<string, string>  $theme_config 
+		 * @return void
+		 */
+		private function setup_loop( array $theme_config ) {
 
-		private function setup_loop( $theme_config ) {
+			/**
+			 * Override default config values
+			 * @var array<string, array> $config
+			 */
 			$config = $this->extend_and_override_defaults( $theme_config );
 
 			// foreach($config as $group => $item ) {
@@ -161,7 +190,14 @@ if ( ! class_exists( 'Pb' ) ) {
 				);
 			}
 		}
-
+		
+		/**
+		 * action_admin_notice
+		 *
+		 * @param  string $message
+		 * @param  string|null $notice_type
+		 * @return void
+		 */
 		public function action_admin_notice( $message, $notice_type = null ) {
 			add_action(
 				'admin_notices',
@@ -170,45 +206,69 @@ if ( ! class_exists( 'Pb' ) ) {
 				}
 			);
 		}
-
+		
+		/**
+		 * HTML wrapper for admin notice message
+		 *
+		 * @param  mixed $message
+		 * @param  string|null $notice_type
+		 * @return void
+		 */
 		private function admin_notice( $message, $notice_type = null ) {
-			if ( ! empty( $notice_type ) ) {
-				$class = $notice_type;
-			} else {
-				$class = 'info';
-			}
 
-			$html_message = sprintf( '<div class="notice is-dismissible notice-' . $class . '">%s</div>', wpautop( $message ) );
-
+			$class = ( ! empty( $notice_type ) ) ? $notice_type : 'info';
+			$html_message = '<div class="notice is-dismissible notice-' . $class . '">' . $message . '</div>';
 			echo wp_kses_post( $html_message );
-		}
 
+		}
+		
+		/**
+		 * Action after theme setup
+		 *
+		 * @param  callable $function
+		 * @return void
+		 */
 		private function action_after_setup( $function ) {
-			add_action(
-				'after_setup_theme',
-				function () use ( $function ) {
-					$function;
-				}
-			);
+			add_action( 'after_setup_theme', function () use ( $function ) { $function(); });
 		}
-
+		
+		/**
+		 * action_enqueue_scripts
+		 *
+		 * @param  callable $function
+		 * @return void
+		 */
 		private function action_enqueue_scripts( $function ) {
-			add_action(
-				'wp_enqueue_scripts',
-				function () use ( $function ) {
-					$function();
-				}
-			);
+			add_action( 'wp_enqueue_scripts', function () use ( $function ) { $function(); } );
 		}
-
-		private function add_support( $feature, $options = null ) {
-			add_theme_support( $feature, $options );
+		
+		/**
+		 * add_support
+		 * @param string $feature
+     * @param string|null $options
+		 * @return void
+		 */
+		private function add_support( $feature, $options = null ) { 
+      add_theme_support( $feature, $options );
 		}
-
+		/**
+		 * remove_support
+		 * @param string $feature
+		 * @return void
+		 */
 		private function remove_support( $feature ) {
 			remove_theme_support( $feature );
 		}
-
+		
+		/**
+		 * add_image_size
+		 *
+		 * @param  string $name
+		 * @param  int $width
+		 * @param  int $height
+		 * @param  bool $crop
+		 * @return object
+		 */
 		private function add_image_size( $name, $width = 0, $height = 0, $crop = false ) {
 			$this->action_after_setup(
 				function () use ( $name, $width, $height, $crop ) {
@@ -217,16 +277,28 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * remove_image_size
+		 *
+		 * @param  string $name
+		 * @return object
+		 */
 		private function remove_image_size( $name ) {
-			$this->action_after_setup(
-				function () use ( $name ) {
-					remove_image_size( $name );
-				}
-			);
+			$this->action_after_setup( function () use ( $name ) { remove_image_size( $name ); } );
 			return $this;
 		}
-
+		
+		/**
+		 * add_style
+		 *
+		 * @param  string $handle
+		 * @param  mixed $src
+		 * @param  array<string> $deps
+		 * @param  bool|string $ver
+		 * @param  string $media
+		 * @return object
+		 */
 		public function add_style( $handle, $src = '', $deps = array(), $ver = false, $media = 'all' ) {
 			$this->action_enqueue_scripts(
 				function () use ( $handle, $src, $deps, $ver, $media ) {
@@ -235,7 +307,17 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * add_script
+		 *
+		 * @param  string $handle
+		 * @param  mixed $src
+		 * @param  array<string> $deps
+		 * @param  bool|string $ver
+		 * @param  bool $in_footer
+		 * @return object
+		 */
 		public function add_script( $handle, $src = '', $deps = array(), $ver = false, $in_footer = true ) {
 			$this->action_enqueue_scripts(
 				function () use ( $handle, $src, $deps, $ver, $in_footer ) {
@@ -244,7 +326,12 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * add_comment_script
+		 *
+		 * @return object
+		 */
 		public function add_comment_script() {
 			$this->action_enqueue_scripts(
 				function () {
@@ -255,7 +342,13 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * remove_style
+		 *
+		 * @param  string $handle
+		 * @return object
+		 */
 		public function remove_style( $handle ) {
 			$this->action_enqueue_scripts(
 				function () use ( $handle ) {
@@ -265,7 +358,13 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * remove_script
+		 *
+		 * @param  string $handle
+		 * @return object
+		 */
 		public function remove_script( $handle ) {
 			$this->action_enqueue_scripts(
 				function () use ( $handle ) {
@@ -275,37 +374,56 @@ if ( ! class_exists( 'Pb' ) ) {
 			);
 			return $this;
 		}
-
-		public function add_nav_menus( $locations = array() ) {
+		
+		/**
+		 * add_nav_menus
+		 *
+		 * @param  array<string> $locations
+		 * @return object
+		 */
+		public function add_nav_menus( array $locations ) {
 			$this->action_after_setup(
-				function () use ( $locations ) {
-					register_nav_menus( $locations );
-				}
+				function () use ( $locations ) { register_nav_menus( $locations ); }
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * add_nav_menu
+		 *
+		 * @param  string $location
+		 * @param  string $description
+		 * @return object
+		 */
 		public function add_nav_menu( $location, $description ) {
 			$this->action_after_setup(
-				function () use ( $location, $description ) {
-					register_nav_menu( $location, $description );
-				}
+				function () use ( $location, $description ) { register_nav_menu( $location, $description ); }
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * remove_nav_menu
+		 *
+		 * @param  string $location
+		 * @return object
+		 */
 		public function remove_nav_menu( $location ) {
 			$this->action_after_setup(
-				function () use ( $location ) {
-					unregister_nav_menu( $location );
-				}
+				function () use ( $location ) { unregister_nav_menu( $location ); }
 			);
 			return $this;
 		}
-
+		
+		/**
+		 * get_menu_items
+		 *
+		 * @param  string $location_id
+		 * @return object
+		 */
 		public function get_menu_items( $location_id ) {
 			//$locations = get_registered_nav_menus();
-			$menus		  = wp_get_nav_menus();
+			$menus = wp_get_nav_menus();
 			$menu_locations = get_nav_menu_locations();
 
 			if ( isset( $menu_locations[ $location_id ] ) && 0 !== $menu_locations[ $location_id ] ) {
@@ -318,36 +436,44 @@ if ( ! class_exists( 'Pb' ) ) {
 				return $menu_items;
 			}
 		}
-
+		
+		/**
+		 * action_extend_head
+		 *
+		 * @param  callable $function
+		 * @return void
+		 */
 		public function action_extend_head( $function ) {
-			add_action(
-				'wp_head',
-				function () use ( $function ) {
-					$function();
-				}
-			);
+			add_action( 'wp_head', function () use ( $function ) { $function(); } );
 		}
-
-		// public function add_site_meta( $key, $value ) {
-		// 	switch ( $key ) {
-		//		 case 'canonical':
-		//			 break;
-		//		 case 'manifest':
-		//			 // mainfest added only to child theme
-		//			 // if (is_child_theme() ) {
-		//				 // TODO: checkout where manifest might be to work properly(in root folder? )
-		//				 echo '<link rel="manifest" href="' . get_template_directory_uri() . '/manifest.json">';
-		//			 // }
-		//			 break;
-		//		 case 'profile':
-		//			 break;
-		//		 case 'pingback':
-		//			 break;
-		//		 case '':
-		//			 break;
-		//		 default:
-		// 	}
-		// }
+		
+		/**
+		 * add_site_meta
+		 *
+		 * @param  mixed $key
+		 * @param  mixed $value
+		 * @return void
+		 */
+		public function add_site_meta( $key, $value ) {
+			switch ( $key ) {
+				 case 'canonical':
+					 break;
+				 case 'manifest':
+					 // mainfest added only to child theme
+					 // if (is_child_theme() ) {
+						 // TODO: checkout where manifest might be to work properly(in root folder? )
+						 echo '<link rel="manifest" href="' . get_template_directory_uri() . '/manifest.json">';
+					 // }
+					 break;
+				 case 'profile':
+					 break;
+				 case 'pingback':
+					 break;
+				 case '':
+					 break;
+				 default:
+			}
+		}
 
 		// public function addAnalytics() {
 
